f750e02ee1d2b004b780c8b2ce0861f6c0a97973
Afzal Siddique <afzal.siddique99@gmailcom>
2020-09-30 16:48:57 +0600

    moutain scenes problem time limit exceeded

--- Problem_Solving_Python/Miscellaneous/moutain_scenes.py
+++ Problem_Solving_Python/Miscellaneous/moutain_scenes.py
@@ -1,25 +1,25 @@
 # https://open.kattis.com/problems/scenes
+# https://www.youtube.com/watch?v=pPgBZqY_Xh0&list=PLDV1Zeh2NRsAsbafOroUBnNV8fhZa7P4u&index=4
 
 
-class Solution:
-    def count_mountain_scenes(self, ribbon, WIDTH, HEIGHT):
-        MOD = 1_000_000_007
-        dp = [[-1] * (ribbon + 1) for _ in range(WIDTH + 1)]  # -1 means null
-        ribbon = min(HEIGHT * WIDTH, ribbon)
-        no_of_plains = ribbon // WIDTH + 1
+def count_mountain_scenes(N, WIDTH, HEIGHT):
+    MOD = 1_000_000_007
+    ribbon_squares = min(HEIGHT * WIDTH, N)
+    dp = [[-1] * (ribbon_squares + 1) for _ in range(WIDTH + 1)]  # -1 means null
+    no_of_plains = ribbon_squares // WIDTH + 1
 
-        def f(width, ribbon):
-            if ribbon < 0:
-                return 0
-            if width > WIDTH:
-                return 1
-            if dp[width][ribbon] != -1:  # not null
-                return dp[width][ribbon]
-            count = 0
-            for height in range(HEIGHT + 1):
-                count += f(width + 1, ribbon - height)
-            dp[width][ribbon] = count
-            return dp[width][ribbon] % MOD
+    def f(width, ribbon):
+        if ribbon < 0:
+            return 0
+        if width > WIDTH:
+            return 1
+        if dp[width][ribbon] != -1:  # not null
+            return dp[width][ribbon]
+        count = 0
+        for height in range(HEIGHT + 1):
+            count += f(width + 1, ribbon - height)
+        dp[width][ribbon] = count % MOD
+        return dp[width][ribbon]
 
-        ans = ((f(1, ribbon) - no_of_plains) + MOD) % MOD
-        return int(ans)
+    ans = ((f(1, ribbon_squares) - no_of_plains) + MOD) % MOD
+    return int(ans)
--- Problem_Solving_Python/Miscellaneous/moutain_scenes_test.py
+++ Problem_Solving_Python/Miscellaneous/moutain_scenes_test.py
@@ -4,25 +4,21 @@ from Miscellaneous.moutain_scenes import *
 
 class MyTestCase(unittest.TestCase):
     def test_1(self):
-        solution = Solution()
-        actual = solution.count_mountain_scenes(25, 5, 5)
+        actual = count_mountain_scenes(25, 5, 5)
         expected = 7770
         self.assertEqual(expected,actual)
 
     def test_2(self):
-        solution = Solution()
-        actual = solution.count_mountain_scenes(15, 5, 5)
+        actual = count_mountain_scenes(15, 5, 5)
         expected = 6050
         self.assertEqual(expected,actual)
 
     def test_3(self):
-        solution = Solution()
-        actual = solution.count_mountain_scenes(10, 10, 1)
+        actual = count_mountain_scenes(10, 10, 1)
         expected = 1022
         self.assertEqual(expected,actual)
 
     def test_4(self):
-        solution = Solution()
-        actual = solution.count_mountain_scenes(4, 2, 2)
+        actual = count_mountain_scenes(4, 2, 2)
         expected = 6
         self.assertEqual(expected,actual)
