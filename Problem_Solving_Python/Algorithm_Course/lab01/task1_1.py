f0b8e2d9beca3eccc782f258b3c56ad893bc72b9
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-06 12:58:33 +0600

    algorithm course lab01 task1_1 complete

--- Problem_Solving_Python/Algorithm_Course/lab01/task1_1.py
+++ Problem_Solving_Python/Algorithm_Course/lab01/task1_1.py
@@ -1,28 +1,57 @@
-def merge_sort(arr):
-    if len(arr) == 1:
-        return arr
-    if len(arr) == 2:
-        if arr[0] > arr[1]:
-            arr[0], arr[1] = arr[1], arr[0]
-        return arr
-    first = merge_sort(arr[:len(arr) // 2])
-    second = merge_sort(arr[len(arr) // 2:])
-    li = []
+def merge_sort(tuple_list):
+    if len(tuple_list) == 1:
+        return tuple_list
+    if len(tuple_list) == 2:
+        if tuple_list[0] > tuple_list[1]:
+            tuple_list[0], tuple_list[1] = tuple_list[1], tuple_list[0]
+        return tuple_list
+    first = merge_sort(tuple_list[:len(tuple_list) // 2])
+    second = merge_sort(tuple_list[len(tuple_list) // 2:])
+    sorted_array = []
     i, j = 0, 0
     while i != len(first) or j != len(second):
-        if first[i] <= second[j]:
-            li.append(first[i])
+        if is_less_than_or_equal(first[i], second[j]):
+            sorted_array.append(first[i])
             i += 1
         else:
-            li.append(second[j])
+            sorted_array.append(second[j])
             j += 1
         if i == len(first):
-            li += second[j:]
+            sorted_array += second[j:]
             break
         if j == len(second):
-            li += first[i:]
+            sorted_array += first[i:]
             break
-    return li
+    return sorted_array
 
-# def is_less_than_or_equal(a, b):
-    
+
+def is_less_than_or_equal(a, b):
+    if a[0] < b[0]:
+        return True
+    elif a[0] == b[0]:
+        if a[1] <= b[1]:
+            return True
+        else:
+            return False
+    elif a[0] > b[0]:
+        return False
+
+
+def generate_tuples(arr):
+    tuple_list = []
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            tuple_list.append((arr[i], arr[j]))
+    return tuple_list
+
+
+def final(arr, k):
+    tuple_list = generate_tuples(arr)
+    sorted_array = merge_sort(tuple_list)
+    return sorted_array[k - 1]
+
+
+n, k = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+result = final(arr, k)
+print(result[0], result[1])
--- Problem_Solving_Python/Algorithm_Course/lab01/task1_1test.py
+++ Problem_Solving_Python/Algorithm_Course/lab01/task1_1test.py
@@ -1,17 +1,68 @@
 import unittest
 from Algorithm_Course.lab01.task1_1 import *
 
+
 class MyTestCase(unittest.TestCase):
-    def test_1(self):
-        arr = [2,6,4,1,6,2]
+    def test_final1(self):
+        arr = [3, 1, 5]
+        k = 2
+        expected = (1, 3)
+        actual = final(arr, k)
+        self.assertEqual(expected, actual)
+
+    def test_final2(self):
+        arr = [1, 1, 3, 3]
+        k = 11
+        expected = (3, 1)
+        actual = final(arr, k)
+        self.assertEqual(expected, actual)
+
+    def test_merge_sort1(self):
+        arr = [(3, 3), (3, 1), (3, 5), (1, 3), (1, 1), (1, 5), (5, 3), (5, 1), (5, 5)]
         expected = sorted(arr)
         actual = merge_sort(arr)
         self.assertEqual(expected, actual)
 
-    def test_2(self):
-        arr = [2, 1, 4, 3, 5, 9, 8]
+    def test_merge_sort2(self):
+        arr = [(1, 1), (1, 1), (1, 3), (1, 3), (1, 1), (1, 1), (1, 3), (1, 3), (3, 1), (3, 1), (3, 3), (3, 3), (3, 1),
+               (3, 1), (3, 3), (3, 3)]
         expected = sorted(arr)
         actual = merge_sort(arr)
+        self.assertEqual(expected, actual)
+
+    def test_is_less_than_or_equal1(self):
+        a = (1, 3)
+        b = (2, 3)
+        expected = True
+        actual = is_less_than_or_equal(a, b)
+        self.assertEqual(expected, actual)
+
+    def test_is_less_than_or_equal2(self):
+        a = (1, 3)
+        b = (1, 3)
+        expected = True
+        actual = is_less_than_or_equal(a, b)
+        self.assertEqual(expected, actual)
+
+    def test_is_less_than_or_equal3(self):
+        a = (1, 2)
+        b = (1, 3)
+        expected = True
+        actual = is_less_than_or_equal(a, b)
+        self.assertEqual(expected, actual)
+
+    def test_is_less_than_or_equal4(self):
+        a = (2, 2)
+        b = (1, 3)
+        expected = False
+        actual = is_less_than_or_equal(a, b)
+        self.assertEqual(expected, actual)
+
+    def test_is_less_than_or_equal5(self):
+        a = (1, 3)
+        b = (1, 1)
+        expected = False
+        actual = is_less_than_or_equal(a, b)
         self.assertEqual(expected, actual)
 
 
