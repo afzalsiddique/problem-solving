a41645d425ead95da2f340b4cbeee90aeab8b995
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-08 05:38:09 +0600

    c problem accepted

--- Problem_Solving_Python/Algorithm_Course/lab01/contest_c.py
+++ Problem_Solving_Python/Algorithm_Course/lab01/contest_c.py
@@ -2,38 +2,17 @@ def num_of_tilings(W) -> int:
     dp = [[0] * 16 for _ in range(W + 1)]
     dp[0][15] = 1
     for i in range(1, W + 1):
-        dp[i][0] += dp[i - 1][0]
-        dp[i][0] += dp[i - 1][3]
-        dp[i][0] += dp[i - 1][9]
-        dp[i][0] += dp[i - 1][12]
-        dp[i][0] += dp[i - 1][15]
-
-        dp[i][3] += dp[i - 1][0]
-        dp[i][3] += dp[i - 1][3]
-        dp[i][3] += dp[i - 1][9]
-        dp[i][3] += dp[i - 1][12]
-        dp[i][3] += dp[i - 1][15]
-
-        dp[i][6] += dp[i - 1][0]
-        dp[i][6] += dp[i - 1][3]
-        dp[i][6] += dp[i - 1][9]
-        dp[i][6] += dp[i - 1][12]
-        dp[i][6] += dp[i - 1][15]
-
-        dp[i][9] += dp[i - 1][6]
-
-        dp[i][12] += dp[i - 1][0]
-        dp[i][12] += dp[i - 1][3]
-        dp[i][12] += dp[i - 1][9]
-        dp[i][12] += dp[i - 1][12]
-        dp[i][12] += dp[i - 1][15]
-
-        dp[i][15] += dp[i - 1][0]
-        dp[i][15] += dp[i - 1][3]
-        dp[i][15] += dp[i - 1][6]
-        dp[i][15] += dp[i - 1][9]
-        dp[i][15] += dp[i - 1][12]
-        dp[i][15] += dp[i - 1][15]
+        # 6 valid states 0,3,6,9,12,15
+        dp[i][0] = dp[i - 1][15]
+        dp[i][3] = dp[i - 1][12] + dp[i - 1][15]
+        dp[i][6] = dp[i - 1][9] + dp[i - 1][15]
+        dp[i][9] = dp[i - 1][6]
+        dp[i][12] = dp[i - 1][3] + dp[i - 1][15]
+        dp[i][15] = dp[i - 1][0] + dp[i - 1][3] + dp[i - 1][6] + dp[i - 1][12] + dp[i - 1][15]
 
     return dp[W][15]
 
+
+for case in range(int(input())):
+    ans = num_of_tilings(int(input()))
+    print("{} {}".format(case + 1, ans))
