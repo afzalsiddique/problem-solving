11364f5973fbecd7745e48276d4a37adf8fd977e
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-07 18:53:34 +0600

    can't convert input as required by the calculate_bananas function

--- Problem_Solving_Python/Algorithm_Course/lab01/contest_a.py
+++ Problem_Solving_Python/Algorithm_Course/lab01/contest_a.py
@@ -1,33 +1,40 @@
-# N = int(input())
-# k = 2 * N - 1
-# dp = [[-1]*N for _ in range(k)]
 def calculate_bananas(a, dp, i, j, k):
     if a[i][j] == 0:
         return 0
-    if i == k-1:
+    if i == k - 1:
         return a[i][j]
 
     if dp[i][j] != -1:
         return dp[i][j]
 
-    bottom = a[i][j] + calculate_bananas(a, dp, i+1, j, k)
-    bottom_right = a[i][j] + calculate_bananas(a, dp, i+1, j+1, k)
+    bottom = a[i][j] + calculate_bananas(a, dp, i + 1, j, k)
+    bottom_right = a[i][j] + calculate_bananas(a, dp, i + 1, j + 1, k)
 
     dp[i][j] = max(bottom, bottom_right)
     return dp[i][j]
 
 
+n = int(input())
+k = 2 * n - 1
+dp = [[-1] * (n+1) for _ in range(k)]
+a = [[0] * (n+1) for _ in range(k)]
 
 
 
-
-
-
-
-# for i in range(N):
-#     templist = list(map(int, input().split()))
-#     length = len(templist)
-#     for j in range(length):
-#         a[i][j] = templist[j]
-#
+for i in range(n):
+    templist = list(map(int, input().split()))
+    tempListlenght = len(templist)
+    for j in range(n+1):
+        if j < tempListlenght:
+            a[i][j] = templist[j]
+print(a)
+for i in range(n, 2*n-1):
+    templist = list(map(int, input().split()))
+    for j in range(n+1):
+        if j < i-n+1 or j >= 2*n-1:
+            continue
+        a[i][j] = templist[j]
+print("\n\n")
+print(a)
+    # for j in range(i-n+1, 2*n-1):
 
