8b6146e64b23a71b137e2eddfb616697b0a130e2
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-09 17:13:36 +0600

    comments added in leetcode 292

--- Problem_Solving_Python/leetcode/leetcode292.py
+++ Problem_Solving_Python/leetcode/leetcode292.py
@@ -1,10 +1,12 @@
 class Solution:
-    memo = []
+    dp = []
 
     def canWinNim(self, n: int) -> bool:
-        self.memo = [[-1] * (n + 1) for _ in range(2)]
+        self.dp = [[-1] * (n + 1) for _ in range(2)]
         return self.helper(n, 1)
 
+    # ID == 0 is opponent
+    # ID ==1 is me
     def helper(self, n, ID):
         if n == 0:
             if ID == 1:
@@ -14,12 +16,12 @@ class Solution:
             if ID == 1:
                 return True
             return False
-        if self.memo[ID][n] != -1:
-            return self.memo[ID][n]
+        if self.dp[ID][n] != -1:
+            return self.dp[ID][n]
 
         next = abs(ID - 1)
-        if ID == 1:
-            self.memo[ID][n] = max(self.helper(n-1, next), self.helper(n-2, next), self.helper(n-3, next))
-        else:
-            self.memo[ID][n] = min(self.helper(n-1, next), self.helper(n-2, next), self.helper(n-3, next))
-        return self.memo[ID][n]
+        if ID == 1: # if it is my turn
+            self.dp[ID][n] = max(self.helper(n - 1, next), self.helper(n - 2, next), self.helper(n - 3, next))
+        else: # if it is opponent's turn
+            self.dp[ID][n] = min(self.helper(n - 1, next), self.helper(n - 2, next), self.helper(n - 3, next))
+        return self.dp[ID][n]
