787384e16f557a893dbbbcb419211abbc37c4632
Afzal Siddique <afzal.siddique99@gmailcom>
2020-11-22 12:13:37 +0600

    leetcode53 solved

--- Problem_Solving_Python/leetcode/leetcode53test.py
+++ Problem_Solving_Python/leetcode/leetcode53test.py
@@ -1,21 +1,21 @@
 import unittest
 from leetcode.leetcode53 import *
 
+
 class MyTestCase(unittest.TestCase):
     def test_1(self):
         solution = Solution()
-        nums = [-2,1,-3,4,-1,2,1,-5,4]
+        nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
         actual = solution.maxSubArray(nums)
         expected = 6
         self.assertEqual(expected, actual)
 
     def test_2(self):
         solution = Solution()
-        nums = [-2,-1]
+        nums = [-2, -1]
         actual = solution.maxSubArray(nums)
         expected = -1
         self.assertEqual(expected, actual)
-
 
     def test_3(self):
         solution = Solution()
