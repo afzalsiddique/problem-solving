943e8aca05c48fe994e1942bf8f7f0d135dc465c
Afzal Siddique <afzal.siddique99@gmailcom>
2020-11-26 23:12:18 +0600

    leetcode3 improved

--- Problem_Solving_Python/leetcode/leetcode3.py
+++ Problem_Solving_Python/leetcode/leetcode3.py
@@ -1,37 +1,26 @@
+from collections import defaultdict
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         n = len(s)
-        if n==0 or n==1:
+        if n == 0 or n == 1:
             return n
-        di = {}
-        for char in s:
-            di[char] = 0
+        di = defaultdict(int)
         left, right = 0, 0
-        maxx = 0
-        di[s[left]]+=1
-        while True:
-            window = s[left:right-left+1]
-            if right == n-1:
-                maxx = max(maxx, right-left+1)
-                break
-            if di[s[right+1]] == 0:
-                di[s[right+1]]+=1
-                right+=1
-                window = s[left:right-left+1]
-                maxx = max(maxx, right-left+1)
-                continue
-            if left == right:
-                di[s[left]]-=1
-                di[s[right+1]]+=1
-                left+=1
-                right+=1
-                window = s[left:right-left+1]
-                maxx = max(maxx, right-left+1)
-                continue
-            if di[s[right+1]] == 1:
-                di[s[left]]-=1
-                left+=1
-                window = s[left:right-left+1]
-                maxx = max(maxx, right-left+1)
-                continue
-        return maxx
+        ans = 0
+        di[s[left]] += 1
+        while right != n - 1:
+            if di[s[right + 1]] == 0:
+                di[s[right + 1]] += 1
+                right += 1
+            elif left == right:
+                di[s[left]] -= 1
+                di[s[right + 1]] += 1
+                left += 1
+                right += 1
+            elif di[s[right + 1]] == 1:
+                di[s[left]] -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
