aaec997c05ff222367910cbe742b08a16827e920
Afzal Siddique <afzal.siddique99@gmail.com>
2023-12-16 11:49:46 +0600

    update on 2023-12-16

--- Problem_Solving_Python/leetcode/lc2251.py
+++ Problem_Solving_Python/leetcode/lc2251.py
@@ -3,22 +3,22 @@ from binary_tree_tester import ser,des,TreeNode; from a_linked_list import make_
 from Problem_Solving_Python.template.binary_tree import deserialize,serialize
 def get_sol(): return Solution()
 class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        start,end=sorted(a for a,b in flowers),sorted(b for a,b in flowers)
+        return [bisect_right(start,t)-bisect_left(end,t) for t in people]
+class Solution2:
+    # prefix sum
     def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
         diff = sortedcontainers.SortedDict({0: 0})
-        diff[4]=1
-        print(diff.bis)
         for i,j in flowers:
             diff[i] = diff.get(i, 0) + 1
             diff[j + 1] = diff.get(j + 1, 0) - 1
 
-        count = list(accumulate(diff.values()))
-        print(diff)
-        print(count)
+        count = list(accumulate(diff.values())) # prefix sum
         res=[]
         for t in persons:
-            tmp1=diff.bisect(t)
-            tmp2=count[tmp1-1]
-            res.append(tmp2)
+            idx=diff.bisect(t)
+            res.append(count[idx-1]) # '-1' because dictionary was initialized with {0:0}
         return res
 
 
@@ -28,8 +28,7 @@ class Tester(unittest.TestCase):
         self.assertEqual([1,2,2,2], get_sol().fullBloomFlowers([[1,6],[3,7],[9,12],[4,13]], [2,3,7,11]))
     def test2(self):
         self.assertEqual([2,2,1], get_sol().fullBloomFlowers([[1,10],[3,3]],  [3,3,2]))
-    def test3(self):
-        self.assertEqual([1,2,2,2], get_sol().fullBloomFlowers([[1,6],[3,8]], [2,3,7,11]))
+    # def test3(self):
     # def test4(self):
     # def test5(self):
     # def test6(self):
