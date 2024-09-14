2f964c05457179e4cbddc64a02eeff494753df3a
afzalsiddique <afzal.siddique99@gmail.com>
2024-09-14 21:31:28 +0600

    update 2024-09-14

---
+++ Problem_Solving_Python/leetcode/lc237.py
@@ -0,0 +1,19 @@
+from itertools import accumulate,permutations; from math import floor,ceil,sqrt; import operator; import random; import string; from bisect import *; from collections import deque, defaultdict, Counter, OrderedDict; from functools import reduce, cache, cmp_to_key; from heapq import heappop,heappush,heapify; import unittest; from typing import List, Optional, Union; from functools import cache; from operator import lt, gt; import sortedcontainers
+from binary_tree_tester import ser,des,TreeNode; from a_linked_list import make_linked_list,ListNode
+from Problem_Solving_Python.template.binary_tree import deserialize,serialize
+def get_sol(): return Solution()
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val=node.next.val
+        node.next=node.next.next
+
+class Tester(unittest.TestCase):
+    def test01(self):
+        ll = make_linked_list([2,4,3])
+        node = ll.next
+        get_sol().deleteNode(node)
+        self.assertEqual(make_linked_list([2,3]),ll)
